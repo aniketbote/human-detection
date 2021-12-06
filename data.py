fe6bea068966cd4d8f5bfe1ab98aeed3944b84e0
Aniket Bote <aniketbote2098@gmail.com>
2021-12-06 17:25:24 -0500

    Created data file for utilities regarding loading data

---
+++ data.py
@@ -0,0 +1,77 @@
+import os
+
+import numpy as np
+from skimage.feature import hog
+from skimage.color import rgb2gray
+from skimage.io import imread
+
+def load_data(path):
+    '''
+    Read image data from directory and compute hog features
+
+    Args: 
+        path: The path string to image dir
+    Returns:
+        matrix: An array containing the hog features of all images in image dir
+        image_list: An list containing all the image names
+    '''
+
+    # Initialize empty array matrix, image_list
+    matrix = []
+    image_list = []
+
+    # Iterate over all the images in path
+    for image in os.listdir(path):
+        # Read and convert the images to grayscale
+        img = rgb2gray(imread(os.path.join(path,image)))
+
+        # Compute hog features
+        fd = hog(img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2))
+
+        # Add hog features to matrix
+        matrix.append(fd)
+        
+        # Add image name to image list
+        image_list.append(image)
+
+
+    # Return matrix, image list
+    return np.array(matrix), image_list
+
+def create_dataset(pos_path, neg_path):
+    '''
+    Args:
+        pos_path: The image directory containing positive images
+        neg_path: The image directory containing negative images
+    Returns:
+        X: Features of all the images
+        y: Labels of all images
+
+    '''
+    # Compute hog features
+    X_pos, x_pos_names = load_data(pos_path)
+
+    # Assign label 1 for positive images
+    y_pos = np.ones((X_pos.shape[0]))
+    
+    # Compute hog features
+    X_neg, x_neg_names = load_data(neg_path)
+
+    # Assign label 0 for negative images
+    y_neg = np.zeros((X_neg.shape[0]))
+
+    # Concatenate all the features/ lables / names
+    X = np.concatenate((X_pos, X_neg))
+    y = np.concatenate((y_pos, y_neg))
+    image_list = x_pos_names + x_neg_names
+
+    # Return the labels
+    return X, y, image_list
+
+
+if __name__ == "__main__":
+    TRAIN_DIR_POS = "data/Training images (Pos)"
+    TRAIN_DIR_NEG = "data/Training images (Neg)"
+    X_train, y_train, image_list = create_dataset(TRAIN_DIR_POS, TRAIN_DIR_NEG)
+    print(X_train.shape, y_train.shape)
+    print(*image_list, sep = '\n')
